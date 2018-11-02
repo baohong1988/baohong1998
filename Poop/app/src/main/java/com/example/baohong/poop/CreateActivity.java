0e089f526ce7f0e67e0ea384d6d9afb4b63619a3
baohong1998 <bao_mu2012@yahoo.com.vn>
2018-11-02 03:34:06 -0400

    android update

--- Poop/app/src/main/java/com/example/baohong/poop/CreateActivity.java
+++ Poop/app/src/main/java/com/example/baohong/poop/CreateActivity.java
@@ -1,13 +1,21 @@
 package com.example.baohong.poop;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CreateActivity extends AppCompatActivity {
+abstract public class CreateActivity extends AppCompatActivity {
+    protected EditText fname, lname, DoneCheck, phone, email, street, city, state, zip;
+    protected String gfname, glname, gEmail, gPhone, gStreet, gCity, gState, gZip;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create);
-    }
 }
+
