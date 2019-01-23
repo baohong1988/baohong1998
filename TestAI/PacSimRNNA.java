a0591f2e425b4c9a923dd517b51f80c9cd4e00b0
baohong1998 <bao_mu2012@yahoo.com.vn>
2019-01-23 16:05:05 -0500

    update AI assignment

--- TestAI/PacSimRNNA.java
+++ TestAI/PacSimRNNA.java
@@ -1,7 +1,6 @@
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import pacsim.BFSPath;
 import pacsim.PacAction;
 import pacsim.PacCell;
@@ -18,6 +17,9 @@ public class PacSimRNNA implements PacAction {
 
     private List<Point> path;
     private List<Point> food;
+    private List<Integer> visited;
+    private List<PointCost> init_pq;
+    private List<SolutionPath> sol;
     private int simTime;
 
     public PacSimRNNA( String fname ) {
@@ -46,6 +48,8 @@ public class PacSimRNNA implements PacAction {
         if( pc == null ) return null;
         if(simTime == 0)
         {
+            //initialize cost table
+            //food is a list of Point
             System.out.println("\nCost Table:\n");
             food = new ArrayList<>();
             food.add(pc.getLoc());
@@ -62,16 +66,45 @@ public class PacSimRNNA implements PacAction {
                 System.out.println();
 
             }
+            //remove starting location from food array
             food.remove(0);
             System.out.println("\n");
+            //print out food array
             System.out.println("Food Array:\n");
-            for(int i=0; i<food.size(); i++)
+            int num_food = food.size();
+            for(int i=0; i<num_food; i++)
             {
                 System.out.printf("%2d : (%d,%d)\n", i, (int) food.get(i).getX(), (int) food.get(i).getY() );
             }
             System.out.println();
+            //cost from starting pos to every food pallet
+            //init_pq is a list of custom class PointCost
+            init_pq = new ArrayList<PointCost>();
             System.out.println("Population at step 1 :");
-           
+            for(int i=0; i<num_food; i++)
+            {
+                PointCost pointcost = new PointCost(food.get(i), i+1, cost[0][i+1]);
+                init_pq.add(pointcost);
+            }
+            Collections.sort(init_pq, new PointCostComparator());
+            Iterator itr = init_pq.iterator();
+            int i = 0;
+            while (itr.hasNext() && i < init_pq.size())
+            {
+                PointCost next = (PointCost) itr.next();
+                int c = next.getCost();
+                Point d = next.getDestination();
+                System.out.printf("%4d : cost=%d : [(%d,%d),%d]", i, c, (int) d.getX()
+                        , (int) d.getY(), c);
+                i++;
+                System.out.println("\n");
+            }
+            //Applying NNA to every vertex
+            sol = new ArrayList<>();
+            for(int i=0; i<num_food; i++)
+            {
+                sol.addAll(NNA(init_pq.get(i).getPosInCost(), cost))
+            }
 
 
         }
@@ -98,5 +131,35 @@ public class PacSimRNNA implements PacAction {
                 ++simTime, pc.getLoc().x, pc.getLoc().y, face );
         return face;
     }
+    public List<SolutionPath> NNA(Point vertex, int[][] cost, )
+    {
 
+    }
+
+}
+class PointCostComparator implements Comparator<PointCost>{
+    public int compare(PointCost p1, PointCost p2)
+    {
+        if(p1.cost > p2.cost)
+            return 1;
+        else if(p1.cost < p2.cost)
+            return -1;
+        return 0;
+    }
+}
+
+class PointCost{
+    public Point destination;
+    public int vertex;
+    public int cost;
+
+    public PointCost(Point destination, int vertex, int cost)
+    {
+        this.vertex = vertex;
+        this.destination = destination;
+        this.cost = cost;
+    }
+    public Point getDestination(){ return this.destination; }
+    public int getCost(){ return this.cost; }
+    public int getVertex() { return this.vertex; }
 }
